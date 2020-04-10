b6dad306919c04d29b5b29c31b08d26a7cf864eb
Jose Luis <joseluisvb@outlook.com>
2020-04-09 20:45:32 -0700

    no islands and waiting message

--- discord/index.js
+++ discord/index.js
@@ -40,11 +40,18 @@ client.on('ready', () => {
 client.on('message', async message => {
   if (!message.content.startsWith(botPrefix) || message.author.bot) return;
   const params = getParams(message.content);
-  const islands = await turnipExchange.getIslands({ ...params });
-  const toSend = prepareIslandsMessage(islands, params);
 
-  await message.reply('Aquí te van las mejores islas: \n\n');
-  await message.channel.send(toSend, { split: true });
+  const waiting = await message.reply('Buscando islas, perame...');
+  const islands = await turnipExchange.getIslands({ ...params });
+  waiting.delete();
+
+  if (islands.length) {
+    const toSend = prepareIslandsMessage(islands, params);
+    await message.reply('Aquí te van las mejores islas: \n\n');
+    await message.channel.send(toSend, { split: true });
+  } else {
+    await message.reply('Oh no... parece que no hay islas con ese precio :cry:');
+  }
 });
 
 client.on('error', error => {
